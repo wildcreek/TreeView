5d5332ba8a54ca622d3f499d85f1a5edda3913e8
huzhicheng <yujihu989@163.com>
2015-06-24 16:11:51 +0800

    添加了ImageButton，listview条目的点击事件和ImageButton的点击事件共存

--- src/com/example/tree_view/SimpleTreeAdapter.java
+++ src/com/example/tree_view/SimpleTreeAdapter.java
@@ -7,23 +7,25 @@ import com.example.tree.bean.TreeListViewAdapter;
 
 import android.content.Context;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
-
- 
+import android.widget.Toast;
 
 public class SimpleTreeAdapter<T> extends TreeListViewAdapter<T> {
-
+	Context context;
 	public SimpleTreeAdapter(ListView mTree, Context context, List<T> datas,
 			int defaultExpandLevel) throws IllegalArgumentException,
 			IllegalAccessException {
 		super(mTree, context, datas, defaultExpandLevel);
+		this.context = context;
 	}
 
 	@Override
-	public View getConvertView(Node node, int position, View convertView,
+	public View getConvertView(final Node node, int position, View convertView,
 			ViewGroup parent) {
 
 		ViewHolder viewHolder = null;
@@ -38,19 +40,26 @@ public class SimpleTreeAdapter<T> extends TreeListViewAdapter<T> {
 					.findViewById(R.id.line_top);
 			viewHolder.line_Bottom = (View) convertView
 					.findViewById(R.id.line_bottom);
+			viewHolder.ib_practice = (ImageButton) convertView
+					.findViewById(R.id.ib_practice);
 			convertView.setTag(viewHolder);
 
 		} else {
 			viewHolder = (ViewHolder) convertView.getTag();
 		}
- 
+
 		viewHolder.icon.setVisibility(View.VISIBLE);
 		viewHolder.line_Top.setVisibility(node.getLineTopVisiblity());
 		viewHolder.line_Bottom.setVisibility(node.getLineBottomVisiblity());
 		viewHolder.icon.setImageResource(node.getIcon());
-		  
 		viewHolder.label.setText(node.getName());
-
+		
+		viewHolder.ib_practice.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				Toast.makeText(context, "该节点的id为"+node.getId(), Toast.LENGTH_SHORT).show();
+			}
+		});
 		return convertView;
 	}
 
@@ -59,6 +68,7 @@ public class SimpleTreeAdapter<T> extends TreeListViewAdapter<T> {
 		TextView label;
 		View line_Top;
 		View line_Bottom;
+		ImageButton ib_practice;
 	}
 
 }
